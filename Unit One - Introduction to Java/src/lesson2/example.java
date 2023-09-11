56acfe1ca5ea23ab22abbc258de9acbbb62b4b0d
Beatrice Milasan <bmilasan@bayviewglen.ca>
2023-09-11 11:13:43 -0400

    more practice

---
+++ Unit One - Introduction to Java/src/lesson2/example.java
@@ -0,0 +1,9 @@
+package lesson2;
+
+public class example {
+    public static void main(String[] args) {
+        System.out.print("I do not fear computers. ");   // Line 1
+        System.out.println("I fear the lack of them.");  // Line 2
+        System.out.println("--Isaac Asimov"); 
+    }
+}
